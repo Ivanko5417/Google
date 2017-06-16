a0380d0e9d6ffc087e703afd2003206d7470ef93
Ivanko5417 <v.drobysh2011@gmail.com>
2017-06-16 20:36:31 +0300

    Add files via upload

---
+++ scripts/test.js
@@ -0,0 +1,16 @@
+function search(){
+  location.href = 'https://www.google.ru/search?hl=ru&q='+document.getElementById('input').value+'&gws_rd=ssl';
+}
+
+function lucky(){
+  location.href ='https://www.google.com/doodles';
+}
+
+function sign()
+{
+  location.href = 'https://accounts.google.com/signin/v2/identifier?hl=ru&passive=true&continue=https%3A%2F%2Fwww.google.by%2F%3Fgfe_rd%3Dcr%26ei%3DmxJEWdeTMY-8zAWe2rHgDw%26gws_rd%3Dssl&flowName=GlifWebSignIn&flowEntry=ServiceLogin';
+}
+
+function apps(){
+  location.href = 'https://www.google.by/intl/ru/options/';
+}
