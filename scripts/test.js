4f9e5d1f03abcc37cd55b47297dbd9c3be312a67
Ivanko5417 <v.drobysh2011@gmail.com>
2017-06-19 00:42:28 +0300

    Add files via upload

--- scripts/test.js
+++ scripts/test.js
@@ -1,16 +1,51 @@
-function search(){
+var appsIsVissible = false;
+var clickOnButton = false;
+var input = document.getElementById('input');
+window.addEventListener('keypress',search(e));
+function search(e){
+  if(!e)
   location.href = 'https://www.google.ru/search?hl=ru&q='+document.getElementById('input').value+'&gws_rd=ssl';
+  else {
+    if(e.keyCode == 13)
+    {
+      search();
+    }
+  }
 }
 
 function lucky(){
   location.href ='https://www.google.com/doodles';
 }
 
-function sign()
-{
+function sign(){
   location.href = 'https://accounts.google.com/signin/v2/identifier?hl=ru&passive=true&continue=https%3A%2F%2Fwww.google.by%2F%3Fgfe_rd%3Dcr%26ei%3DmxJEWdeTMY-8zAWe2rHgDw%26gws_rd%3Dssl&flowName=GlifWebSignIn&flowEntry=ServiceLogin';
 }
-
+function switchApps()
+{
+  let elemApps = document.getElementById('appsId');
+  if(!appsIsVissible) {
+      elemApps.style.display = 'block';
+      appsIsVissible = true;
+  }
+  else {
+      appsIsVissible = false;
+      elemApps.style.display = 'none';
+  }
+}
 function apps(){
-  location.href = 'https://www.google.by/intl/ru/options/';
+  clickOnButton = true;
+  switchApps();
+}
+function bodyClick() {
+  if (clickOnButton) {
+    clickOnButton = false;
+  }
+  else {
+    if(appsIsVissible)
+    switchApps();
+  }
+}
+function divApps()
+{
+  clickOnButton = true;
 }
